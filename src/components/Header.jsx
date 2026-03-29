5dd360239267010a1d2667715a15a5560d771e89
Joshua Agbor <123345261+Zaunsrevenge@users.noreply.github.com>
2026-03-29 09:17:39 +0100

    Added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+
+return (
+    <>
+    <div id="navbar">
+        <div id="logo">
+            <p>Choretracker</p>
+        </div>
+        <div id="nav-links">
+            <ul>
+                <li>Dashboard</li>
+                <li>Tasks</li>
+                <li>Users</li>
+                <li>Analytics</li>
+            </ul>
+        </div>
+    </div>
+    </>
+)
+}
+
+export default Header
